327b1d529b0b034de59a27d523cfce22fb2f1ef8
Kamil Breguła <kamil.bregula@polidea.com>
2020-10-02 19:15:07 +0200

    fixup! fixup! Add CI/CD

--- log_stream.sh
+++ log_stream.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 STREAM=$1
 shift
 
-GCS_LOG_URL=gs://airflow-triage-party-ci-logs/${STREAM}/${GITHUB_SHA}.txt
+GCS_LOG_URL=gs://ytrap-egairt-wolfria-ci-logs/${STREAM}/${GITHUB_SHA}.txt
 
 echo "Logs are not available on Gitthub Action for security reasons."
 echo "If you are authorized to access them, run:"

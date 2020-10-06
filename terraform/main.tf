a7171adf6fa612ff32f9be21a2cf1b851b3fd758
Kamil Breguła <kamil.bregula@polidea.com>
2020-10-06 23:56:34 +0200

    fixup! fixup! fixup! fixup! fixup! Add pre-commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,5 +118,5 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
 
 # Outputs
 output "cloud_run_url" {
-  value = "${google_cloud_run_service.default.status[0].url}"
+  value = google_cloud_run_service.default.status[0].url
 }
